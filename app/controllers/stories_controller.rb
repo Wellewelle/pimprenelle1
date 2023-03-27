40567f935eb68c4c75ce9e26d71d24d690b127c7
Clement Avignon <clementavignon@gmail.com>
2023-03-27 18:16:33 +0200

    added the stories controller
    Signed-off-by: Clement Avignon <clementavignon@gmail.com>

---
+++ app/controllers/stories_controller.rb
@@ -0,0 +1,32 @@
+class StoriesController < ApplicationController
+  def index
+    @stories = Story.all
+  end
+
+  def show
+    @story = Story.find(params[:id])
+  end
+
+  def new
+    @story = Story.new
+  end
+
+  def create
+    @story = Story.new(story_params)
+    @story.user_id = current_user.id
+    if @story.save
+      redirect_to stories_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def search
+  end
+
+  private
+
+  def story_params
+    params.require(:story).permit(:title, :summary, :content, :tag, :rating, :age, :length, :user_id)
+  end
+end
