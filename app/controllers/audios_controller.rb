1f527f1587eddebb9153e77f9f68883747b35a06
Nawelle Congard <nawelle.congard@gmail.com>
2023-03-27 18:05:32 +0200

    controllers users et audios + new pour l'audio

---
+++ app/controllers/audios_controller.rb
@@ -0,0 +1,19 @@
+class AudiosController < ApplicationController
+  def new
+    @story = Story.find(params[:story_id])
+    @audio = Audio.new
+  end
+
+  def create
+    @audio = Audio.new(audio_params)
+    @raudio.story = @story
+    @audio.save
+    # redirect_to
+  end
+
+  private
+
+  def audio_params
+    # params.require(:audio).permit(:)
+  end
+end
