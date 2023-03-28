8837f466deb366e5b73d8212092ff18910cabd9e
Nawelle Congard <nawelle.congard@gmail.com>
2023-03-28 14:17:41 +0200

    profile page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
 
+  get '/profile', to: 'users#profile'
+
   resources :stories, only: [:index, :show, :new, :create, :search, :edit, :update, :destroy]
 
   resources :favorites, only: :index
