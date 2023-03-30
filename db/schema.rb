585e0dbb50f4c5d6c73a04645764eea00d6a6560
Ndwali Mwenenge <azer8663@outlook.com>
2023-03-30 15:35:00 +0100

    .

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema[7.0].define(version: 2023_03_30_122359) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -67,7 +66,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_30_122359) do
     t.text "content"
     t.string "genre"
     t.integer "rating", default: 0
-    t.integer "age"
+    t.string "age"
     t.integer "length"
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
