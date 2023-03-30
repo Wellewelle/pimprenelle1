e2107db2da6ef052c5799a84d95a44bd439a8d8f
Nawelle Congard <nawelle.congard@gmail.com>
2023-03-30 11:11:40 +0200

    changements à vérifier

--- app/javascript/controllers/toggle_controller.js
+++ app/javascript/controllers/toggle_controller.js
@@ -2,7 +2,13 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="toggle"
 export default class extends Controller {
+  static targets = ["toggleCategories"]
+
   connect() {
     console.log("Hello from toggle_controller.js")
+  }
+
+  toggle() {
+    this.toggleCategoriesTarget.classList.toggle("d-none");
   }
 }
