fc5b8e7d09a73ceb0ef7c3f4f9ebd37ce3c5e04e
Clement Avignon <clementavignon@gmail.com>
2023-03-30 11:00:18 +0200

    autocomplete done but no front end
    Signed-off-by: Clement Avignon <clementavignon@gmail.com>

--- app/models/story.rb
+++ app/models/story.rb
@@ -2,4 +2,11 @@ class Story < ApplicationRecord
   belongs_to :user
   has_many :favorites, through: :users
   # has_many :favorited_by, through: :favorites, source: :user
+
+  include PgSearch::Model
+  pg_search_scope :search_by_title_and_tag,
+    against: [ :title, :tag ],
+    using: {
+      tsearch: { prefix: true }
+    }
 end
