55f5b0e6501edc8c5558f739a96c558d0205ecc4
Nawelle Congard <nawelle.congard@gmail.com>
2023-03-31 13:11:22 +0200

    filtres

--- app/javascript/controllers/toggle_controller.js
+++ app/javascript/controllers/toggle_controller.js
@@ -1,18 +1,29 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["toggleCategories", "toggleInIndex"]
+  static targets = ["toggleCategories", "toggleInIndex", "onClick"]
 
   connect() {
     console.log("Hello from toggle_controller.js")
   }
 
-  toggle() {
+  toggle(event) {
     event.preventDefault()
     this.toggleCategoriesTarget.classList.toggle("d-none");
   }
 
   toggle_in_index() {
     this.toggle_in_index.classList.toggle("d-none");
+  }
+
+  desactivation() {
+  console.log("désactivation hello!")
+  this.onClickTarget.classList.remove("active");
+  }
+
+  activation(event) {
+    console.log("activation hello!")
+    event.preventDefault()
+    this.onClickTarget.classList.toggle("d-none")
   }
 }
